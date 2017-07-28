45e3e86b5ee6f0f9ba263335bc73c8c7ab03bf2a
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-07-28 09:29:20 +0500

    Initial commit

---
+++ app/src/main/java/com/example/abdul_wahab/networkingsummer17/models/Student.java
@@ -0,0 +1,13 @@
+package com.example.abdul_wahab.networkingsummer17.models;
+
+/**
+ * Created by Abdul-Wahab on 7/26/2017.
+ */
+
+public class Student {
+
+    int id;
+    String first_name;
+    boolean gender;
+    String imageurl;
+}
